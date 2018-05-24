3dab8165adf24707a12b79481861972c12af825c
Daniel Strano <stranoj@gmail.com>
2018-05-23 22:19:56 -0400

    Single gate vs. full width

--- qubitGraph.R
+++ qubitGraph.R
@@ -12,12 +12,14 @@ options(scipen=4, digits=3)
 eqTable <- data.frame(MinQubit=integer(), Base=numeric(), Intercept=numeric(), RSqr=numeric(), BaseP=numeric(), InterceptP=numeric(), ModelP=numeric())
 
 # Load data
-testData <- t(read.csv(file="/home/iamu/qrack_benchmarks/qenginecpu/cland.csv", header=TRUE, sep=","))
-firstTrendPoint <- 10
+testData <- read.csv(file="/home/iamu/GitHub/qrack_benchmark_dump/x_all.csv", header=TRUE, sep=",")
+names(testData) <- c("Count", "AvgT",	"SDT", "Q0", "Q1", "Q2", "Q3", "Q4", "Type")
+firstTrendPoint <- 1
+lastTrendPoint <- 22
 
 # Freely varied log base scaling:
-modelY <- log(testData[2,firstTrendPoint:22])
-modelX <- testData[1,firstTrendPoint:22]
+modelY <- log(testData[2,firstTrendPoint:lastTrendPoint])
+modelX <- testData[1,firstTrendPoint:lastTrendPoint]
 exponential.model <- lm(modelY~modelX)
 modelSummary<-summary(exponential.model)
 modelSummary
@@ -38,21 +40,30 @@ eqTable <- rbind(eqTable, eqRow)
 
 qExpTrend <- exp(predict(exponential.model))
 boxplot(testData[c(4,5,6,7,8),], log="y", names=testData[1,], main="X Time vs. No. of Qubits", xlab="No. of Qubits", ylab="Time (ms)")
-lines(testData[1,firstTrendPoint:22] - testData[1,1] + 1, qExpTrend,lwd=2, col = "red", xlab = "No. of Qubits", ylab = "Time (ms)")
+lines(testData[1,firstTrendPoint:lastTrendPoint] - testData[1,1] + 1, qExpTrend,lwd=2, col = "red", xlab = "No. of Qubits", ylab = "Time (ms)")
 # coords <- locator()
 text(x=coords$x, y=coords$y, labels=paste('t ≈ exp[', round(coefs[[2]], 2), '( n - ', -round(coefs[[1]] / coefs[[2]], 2), ')]'))
 
 # Exact log(2) scaling fixed:
-modelY <- (log(testData[2,firstTrendPoint:22]) - (log(2) * testData[1,firstTrendPoint:22]))
-modelX <- rep(1, times=length(testData[1,firstTrendPoint:22]))
+modelY <- (log(testData[2,firstTrendPoint:lastTrendPoint]) - (log(2) * testData[1,firstTrendPoint:lastTrendPoint]))
+modelX <- rep(1, times=length(testData[1,firstTrendPoint:lastTrendPoint]))
 exponential.model <- lm(modelY~modelX)
 summary(exponential.model)
 coefs <- coef(exponential.model)
 
-qExpTrend <- exp(predict(exponential.model) + (log(2) * testData[1,firstTrendPoint:22]))
+qExpTrend <- exp(predict(exponential.model) + (log(2) * testData[1,firstTrendPoint:lastTrendPoint]))
 boxplot(testData[c(4,5,6,7,8),], log="y", names=testData[1,], main="X Time vs. No. of Qubits", xlab="No. of Qubits", ylab="Time (ms)")
-lines(testData[1,firstTrendPoint:22] - testData[1,1] + 1, qExpTrend,lwd=2, col = "red", xlab = "No. of Qubits", ylab = "Time (ms)")
+lines(testData[1,firstTrendPoint:lastTrendPoint] - testData[1,1] + 1, qExpTrend,lwd=2, col = "red", xlab = "No. of Qubits", ylab = "Time (ms)")
 # coords <- locator()
 text(x=coords$x, y=coords$y, labels=paste('t ≈ 2^( n - ', -round(coefs[[1]] / log(2), 2), ')'))
+
+require(ggplot2)
+ggplot(testData, aes(x=factor(Count), y=AvgT, colour = factor(Type))) +
+  scale_colour_discrete(name = "Engine Type") +
+  geom_boxplot(outlier.size=0, fill = "white", position="identity", alpha=.5)  +
+  stat_summary(fun.y=median, geom="line", aes(group=factor(Type)), size=1) + scale_y_log10() +
+  ggtitle ("N CNOT Gates on N Qubits") +
+  xlab("N (No. of Qubits)") +
+  ylab("Average Time (ms)")
 
 orderedEqTable <- eqTable[order(Intercept, Base, MinQubit),]
