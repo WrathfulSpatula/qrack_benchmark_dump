449946aef6d7b78b8d75b26b9ba702a5411eddaa
Daniel Strano <stranoj@gmail.com>
2021-10-17 11:05:17 -0400

    Add heatmap() example

--- generate_graphs_from_samples.r
+++ generate_graphs_from_samples.r
@@ -44,6 +44,25 @@ alldata <- rbind(alldata, avgtime)
 names(alldata) <- c("Qubits", "Time (s)", "Simulator")
 ggplot(alldata, aes(`Qubits`, `Time (s)`, colour = `Simulator`)) + ggtitle("N-qubit random circuit to depth=20, [SWAP,CZ,CX,CCX]/[H,X,Y,Z,T]") + xlab("Qubits") + ylab("Time (s)") + labs(caption="All series are GPU simulators on an Alienware 17, BIOS version 1.8.0, Ubuntu 20.04,\nIntel(R) Core(TM) i9-10980HK CPU @ 2.40GHz, NVIDIA GeForce RTX 2070 Super") + scale_x_continuous(breaks=c(4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28)) + scale_y_log10() + geom_line()
 
+rawdata <- read.csv(file="/home/iamu/GitHub/qrack_benchmark_dump/qrack_benchmarks_10-10-21/pyqrack_random_circuit.csv")
+rawdata <- rawdata[ , 2:4]
+names(rawdata) <- c("Qubits", "Depth", "Seconds")
+avgtime <- aggregate(x = rawdata$Seconds, by = list(rawdata$Qubits, rawdata$Depth), FUN = mean)
+names(avgtime) <- c("Qubits", "Depth", "Seconds")
+widths <- unique(avgtime$Qubits)
+depths <- unique(avgtime$Depth)
+mtrx <- matrix(nrow=length(widths), ncol=length(depths))
+rownames(mtrx) <- widths
+colnames(mtrx) <- depths
+row <- 1
+for (width in 1:length(widths)) {
+  for (depth in 1:length(depths)) {
+    mtrx[width, depth] <- avgtime[row, 3]
+    row = row + 1
+  }
+}
+heatmap(mtrx, Rowv=NA, Colv=NA)
+
 rawdata <- read.csv(file="/home/iamu/GitHub/qrack_benchmark_dump/qrack_benchmarks_10-10-21/pyqrack_sycamore.csv")
 rawdata <- rawdata[ which(rawdata$depth == 20), ]
 avgtime <- aggregate(x = rawdata$time, by = list(rawdata$num_qubits), FUN = mean)
